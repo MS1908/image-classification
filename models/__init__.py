3a87b85f6ca5b990ec090e217265267b6828c5fb
Ngô Quang Minh 54 (AI) <minhnq54@fpt.com.vn>
2023-10-06 14:16:33 +0700

    Add support for training HuggingFace model from scratch

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,2 @@
 from .timm_models import timm_models_factory
+from .hf_models import hf_models_factory
--- models/hf_models.py
+++ models/hf_models.py
@@ -7,7 +7,8 @@ def hf_models_factory(
     id2label,
     label2id,
     trust_remote_code=False,
-    use_hf_pretrain=False
+    use_hf_pretrain=False,
+    ignore_mismatched_sizes=True
 ):
     if use_hf_pretrain:
         config = AutoConfig.from_pretrained(
@@ -15,4 +16,37 @@ def hf_models_factory(
             num_labels=len(labels),
             i2label=id2label,
             label2id=label2id,
+            finetuning_task="image-classification",
+            trust_remote_code=trust_remote_code
         )
+
+        model = AutoModelForImageClassification.from_pretrained(
+            model_name_or_path,
+            from_tf=bool(".ckpt" in model_name_or_path),
+            config=config,
+            ignore_mismatched_sizes=ignore_mismatched_sizes,
+            trust_remote_code=trust_remote_code,
+        )
+
+    else:
+        config = AutoConfig.from_pretrained(
+            model_name_or_path,
+            num_labels=len(labels),
+            i2label=id2label,
+            label2id=label2id,
+            trust_remote_code=trust_remote_code
+        )
+
+        model = AutoModelForImageClassification.from_config(config)
+
+    return config, model
+
+
+if __name__ == '__main__':
+    config, model = build_hf_model(
+        model_name_or_path='microsoft/swin-tiny-patch4-window7-224',
+        labels=['a', 'b'],
+        id2label={0: 'a', 1: 'b'},
+        label2id={'a': 0, 'b': 1},
+        use_hf_pretrain=True
+    )
